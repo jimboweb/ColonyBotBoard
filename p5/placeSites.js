06b52ae79a1c76f8b6df67c5507b5201741c19eb
jimboweb <jimboweb@gmail.com>
2019-05-13 17:41:17 -0400

    generate random connections

--- p5/placeSites.js
+++ p5/placeSites.js
@@ -21,22 +21,49 @@ semiGaussianRandom=(min,max,depth=4)=>{
     return Math.floor(total/depth);
 }
 
-function generateSites(averageFrequency,boardWidth=600,boardHeight=300,hDiv=8,vDiv=4){
-    let lastIndex=0;
+function generateSites(averageFrequency,connectionFrequecy=0.25,boardWidth=600,boardHeight=300,horizontalDivisions=8,verticalDivisions=4){
+    let nextIndex=0;
     let sites = [];
-    let xBlockSize = boardWidth/hDiv;
-    let yBlockSize = boardHeight/vDiv
-    for(let xMin=0;xMin<boardWidth;xMin+=hDiv){
-        for(let yMin=0;yMin<boardHeight;yMin+=vDiv){
+    let xBlockSize = boardWidth/horizontalDivisions;
+    let yBlockSize = boardHeight/verticalDivisions;
+    let segments = new Array(horizontalDivisions);
+    segments.fill(new Array(verticalDivisions));
+    for(let col=0;col<horizontalDivisions;col++){
+        const xMin = col*xBlockSize;
+        for(let row=0;row<verticalDivisions;row++){
+            const yMin = row*yBlockSize;
             const numSites = semiGaussianRandom(2,5);
+            segments[col][row]=[];
             for(let siteNum=0;siteNum<numSites;siteNum++){
                 const xCoord = randInt(xMin,xMin+xBlockSize);
                 const yCoord = randInt(yMin,yMin+yBlockSize);
-                sites.push(new Site(lastIndex++,[],{x:xCoord,y:yCoord},[]))
+               segments[col][row].push(nextIndex);
+               sites.push(new Site(nextIndex++,[],{x:xCoord,y:yCoord},[]))
             }
         }
     }
-    //todo 190513: create adjacents
+    for(var col = 0;col<segments.length-1;col++){
+        for(let row=0;row<col.lenghth-1;row++){
+            segments[col][row].forEach(
+                siteIndex=>{
+                    for(let i=col;i<col+1;i++){
+                        for(let j=row;j<row+1;j++){
+                            segments[i][j].forEach(
+                                otherSiteIndex=>{
+                                    if(Math.random()<connectionFrequecy){
+                                        const site = sites[col][row];
+                                        const otherSite = sites[i][j];
+                                        site.adjacent.push(otherSiteIndex);
+                                        otherSite.adjacent.push(siteIndex);
+                                    }
+                                }
+                            )
+                        }
+                    }
+                }
+            )
+        }
+    }
     return sites;
 }
 
